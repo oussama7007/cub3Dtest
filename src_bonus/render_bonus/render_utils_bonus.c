6cf4d102a3bc0aba1cb8ce191b19829e66b2708e
Biman Kolani <bkolani@e3r1p12.1337.ma>
2025-09-07 19:25:48 +0100

    mini mpa

--- src_bonus/render_bonus/render_utils_bonus.c
+++ src_bonus/render_bonus/render_utils_bonus.c
@@ -3,17 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   render_utils_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bkolani <bkolani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 15:21:18 by bkolani           #+#    #+#             */
-/*   Updated: 2025/09/07 17:18:53 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/09/07 19:25:01 by bkolani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d_bonus.h"
 
-#define MINIMAP_RADIUS 60
-#define MINIMAP_SCALE 20
+#define MINIMAP_RADIUS 100
+#define MINIMAP_SCALE 10
+
+void	draw_square(t_img *img, int x, int y, int size, int color)
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			put_pixel(img, x + j, y + i, color);
+		}
+	}
+}
+
+void	draw_square_(t_img *img, int x, int y, int size, int color)
+{
+	for (int i = -size / 2; i < size; i++)
+	{
+		for (int j =- size / 2; j < size; j++)
+		{
+			put_pixel(img, x + j, y + i, color);
+		}
+	}
+}
 
 // Filled circle
 int draw_filled_circle(t_img *img, int cx, int cy, int r, int color)
@@ -51,7 +73,7 @@ int draw_line(t_img *img, int x0, int y0, int x1, int y1, int color)
 
     while (1)
     {
-        put_pixel(img, x0, y0, color);
+        put_pixel(img, x0 + 4, y0 + 4, color);
         if (x0 == x1 && y0 == y1)
             break;
         e2 = 2 * err;
@@ -99,32 +121,68 @@ int	check_fov_params(t_game *game, t_fov *fov)
 	return (0);
 }
 
-int	draw_mini_map_fov(t_game *game)
+int	draw_mini_map_fov(t_game *game, int center_x, int center_y)
 {
-	t_fov	*fov;
-	double	r;
-	double	angle;
+    t_fov	*fov;
+    double	r;
+    double	angle;
 
-	fov = NULL;
-	init_fov(game, &fov);
-	r = 0;
-	angle = fov->start_angle;
-	while (angle <= fov->end_angle)
-	{
-		r = 0;
-		while (r <= fov->range)
-		{
-			init_fov_in_loop(fov, angle, r);
-			if (check_fov_params(game, fov))
-				break ;
-			if (put_pixel(&game->mlx.screen, fov->x, fov->y, 0xf5b342))
-				return (-1);
-			r += 1.0;
-		}
-		angle += fov->step;
-	}
-	return (0);
+    fov = NULL;
+    init_fov(game, &fov);
+    r = 0;
+    angle = fov->start_angle;
+    while (angle <= fov->end_angle)
+    {
+        r = 0;
+        while (r <= fov->range)
+        {
+            init_fov_in_loop(fov, angle, r);
+            if (check_fov_params(game, fov))
+                break;
+
+            // conversion monde → minimap
+            double dx = (fov->x / (double)CELL_SIZE - game->config.player.pos.x) * MINIMAP_SCALE;
+            double dy = (fov->y / (double)CELL_SIZE - game->config.player.pos.y) * MINIMAP_SCALE;
+
+            int screen_x = center_x + (int)dx;
+            int screen_y = center_y + (int)dy;
+
+            if (dx * dx + dy * dy <= MINIMAP_RADIUS * MINIMAP_RADIUS)
+                put_pixel(&game->mlx.screen, screen_x, screen_y, 0xf5b342);
+
+            r += 1.0;
+        }
+        angle += fov->step;
+    }
+    return (0);
 }
+
+// int	draw_mini_map_fov(t_game *game)
+// {
+// 	t_fov	*fov;
+// 	double	r;
+// 	double	angle;
+
+// 	fov = NULL;
+// 	init_fov(game, &fov);
+// 	r = 0;
+// 	angle = fov->start_angle;
+// 	while (angle <= fov->end_angle)
+// 	{
+// 		r = 0;
+// 		while (r <= fov->range)
+// 		{
+// 			init_fov_in_loop(fov, angle, r);
+// 			if (check_fov_params(game, fov))
+// 				break ;
+// 			if (put_pixel(&game->mlx.screen, fov->x, fov->y, 0xf5b342))
+// 				return (-1);
+// 			r += 1.0;
+// 		}
+// 		angle += fov->step;
+// 	}
+// 	return (0);
+// }
 
 int	draw_mini_map_check(t_game *game, int map_x, int map_y)
 {
@@ -157,7 +215,7 @@ int draw_mini_map(t_game *game, t_img screen)
     int center_y = HEIGHT - MINIMAP_RADIUS - 20;
 
     // background circle
-    draw_filled_circle(&screen, center_x, center_y, MINIMAP_RADIUS, 0xFFFFFFCC);
+    draw_filled_circle(&screen, center_x, center_y, MINIMAP_RADIUS + 10, 0x528697);
 
     for (int y = 0; y < game->config.map.height; y++)
     {
@@ -172,21 +230,27 @@ int draw_mini_map(t_game *game, t_img screen)
             if (dx*dx + dy*dy <= MINIMAP_RADIUS * MINIMAP_RADIUS)
             {
                 if (game->config.map.grid[y][x] == '1')
-                    put_pixel(&screen, screen_x, screen_y, 0xAAAAAA); // wall
+					draw_square(&screen, screen_x, screen_y, 9, 0x000000);
+                    // put_pixel(&screen, screen_x, screen_y, 0xAAAAAA); // wall
 				else if (game->config.map.grid[y][x] == '3')
-					put_pixel(&screen, screen_x, screen_y, 0xFF0000); // item
+					draw_square_(&screen, screen_x, screen_y, 4, 0xFFFF00);
+					// put_pixel(&screen, screen_x, screen_y, 0xFF0000); // item
 				else if (game->config.map.grid[y][x] == '4')
-					put_pixel(&screen, screen_x, screen_y, 0x00FF00); // item
-                else
-                    put_pixel(&screen, screen_x, screen_y, 0x333333); // floor
+					draw_square(&screen, screen_x, screen_y, 8, 0xAAAAAA);
+                    // put_pixel(&screen, screen_x, screen_y, 0x00FF00); // item	
+                // else
+				// 	draw_square(&screen, screen_x, screen_y, 8, 0x333333);
+                    // put_pixel(&screen, screen_x, screen_y, 0x333333); // floor
             }
         }
     }
 
     // player
-    draw_circle(&screen, center_x, center_y, 3, 0xFF0000);
+    // draw_circle(&screen, center_x, center_y, 3, 0x000000);
+	draw_square(&screen, center_x, center_y, 9, 0x0000FF);
 
     // facing direction
+	// draw_mini_map_fov(game, center_x, center_y);
 	int dir_x = (int)(game->config.player.dir.x * 15);
 	int dir_y = (int)(game->config.player.dir.y * 15);
 	draw_line(&screen, center_x, center_y,
