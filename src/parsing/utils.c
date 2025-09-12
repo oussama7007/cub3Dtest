a9a0f3a06386e3624c085bb90a58bc940b33d841
oussama7007 <oussamatachakourt@gmail.com>
2025-09-12 17:12:57 +0100

    s

--- src/parsing/utils.c
+++ src/parsing/utils.c
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 13:25:20 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/09/11 12:24:23 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/09/12 16:45:11 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,8 @@ int	check_middle_rows(t_map map, int i)
 	int	last_char_idx;
 
 	last_char_idx = ft_strlen(map.grid[i]) - 1;
-	if ((map.grid[i][0] != '1' && !ft_isspace(map.grid[i][0]))
-		|| (map.grid[i][last_char_idx] != '1'
-		&& !ft_isspace(map.grid[i][last_char_idx])))
-		return (print_err("Map error: invalid walls!\n"));
+	if ((map.grid[i][0] != '1' && !ft_isspace(map.grid[i][0])) || (map.grid[i][last_char_idx] != '1' && !ft_isspace(map.grid[i][last_char_idx])))
+		return (print_err("Map error: invalid -----  walls!\n"));
 	return (0);
 }
 
